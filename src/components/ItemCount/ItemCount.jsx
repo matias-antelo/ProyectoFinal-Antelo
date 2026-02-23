85a91cb4ca5e078d585f4c44a46051c7623a8be9
nanontelo87 <anteloma87@gmail.com>
2026-02-23 20:11:52 -0300

    actualizacion de itemCount

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -21,25 +21,5 @@ const ItemCount = ({ stock, onAdd }) => {
     </div>
   );
 };
-  /*const decrement = () => {
-    if (count > 1) setCount(count - 1);
-  };
-
-  const increment = () => {
-    if (count < stock) setCount(count + 1);
-  };
-
-  return (
-    <div className="item-count">
-      <div className="counter">
-        <button onClick={decrement}>-</button>
-        <span>{count}</span>
-        <button onClick={increment}>+</button>
-      </div>
-
-      <button className="add-to-cart">Agregar al carrito</button>
-    </div>
-  );
-};*/
-
+  
 export default ItemCount;
